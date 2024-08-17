095dbadffbf2457716db57b04fef2c899d21d99d
HyemiKim <hams2401@naver.com>
2024-08-17 15:04:32 +0900

    2024-08-17-Sat

--- My Quiz Page/index.js
+++ My Quiz Page/index.js
@@ -1,80 +1,52 @@
 let ans1 = '';
 let ans2 = '';
 let ans3 = '';
+let score = 0;
 
-function onClickQuiz1(e) {
+function onClickQuiz(e, selector) {
     let chBox = e.target;
     if (chBox.checked) {
-        let boxes = document.querySelectorAll("#quiz1 input");
+        let boxes = document.querySelectorAll(selector);
         boxes.forEach(b => {
             if(b != chBox) {
                 b.checked = false;
             }
         })
-        ans1 = chBox.value;
+        return chBox.value;
     } else {
-        ans1 = ''
+        return '';
     }
 };
 
-let answers1 = document.querySelectorAll("#quiz1 input");
-answers1.forEach(a => a.addEventListener('click', onClickQuiz1))
-
-function onClickQuiz2(e) {
-    let chBox = e.target;
-    if (chBox.checked) {
-        let boxes = document.querySelectorAll("#quiz2 input");
-        boxes.forEach(b => {
-            if(b != chBox) {
-                b.checked = false;
-            }
-        })
-        ans2 = chBox.value;
-    } else {
-        ans2 = ''
-    }
-};
-
-let answers2 = document.querySelectorAll("#quiz2 input");
-answers2.forEach(a => a.addEventListener('click', onClickQuiz2))
-
-function chColor(actual, expected, selector, score) {
+function calScore(actual, expected, selector) {
     if(actual == expected) {
         score++;
         document.querySelector(selector).style.color='blue';
     } else {
         document.querySelector(selector).style.color='red';
     }
-    return score;
 }
 
 function onClickSubmit() {
-    let score = 0;
     ans3 = document.querySelector('#quiz3 input').value;
-    score = chColor(ans1, '분홍', '#quiz1', score);
-    score = chColor(ans2, '22', '#quiz2', score);
-    score = chColor(ans3, 'AB', '#quiz3', score);
-    // if(ans1 == '분홍') {
-    //     score++;
-    //     document.querySelector("#quiz1").style.color = 'blue';
-    // } else {
-    //     document.querySelector("#quiz1").style.color = 'red';
-    // }
-    // if(ans2 == '22') {
-    //     score++;
-    //     document.querySelector("#quiz2").style.color = 'blue';
-    // } else {
-    //     document.querySelector("#quiz2").style.color = 'red';
-    // }
-    // if(ans3 == 'AB') {
-    //     score++;
-    //     document.querySelector("#quiz3").style.color = 'blue';
-    // } else {
-    //     document.querySelector("#quiz3").style.color = 'red';
-    // }
+    calScore(ans1, '분홍', '#quiz1');
+    calScore(ans2, '22', '#quiz2');
+    calScore(ans3, 'AB', '#quiz3');
 
     alert(`score : ${score} / 3`);   
+
+    score = 0;
 };
+
+let answers1 = document.querySelectorAll("#quiz1 input");
+answers1.forEach(a => a.addEventListener('click', e => {
+    ans1 = onClickQuiz(e, '#quiz1 input');
+}));
+
+let answers2 = document.querySelectorAll("#quiz2 input");
+answers2.forEach(a => a.addEventListener('click', e => {
+    ans2 = onClickQuiz(e, '#quiz2 input');
+}));
 
 let submit = document.querySelector('#submit');
 submit.addEventListener('click', onClickSubmit);
